e796c56ad3e694871dc2ea47431f74ebfa27dde0
Jashanwebsite <jashandeepsingh61124@gmail.com>
2023-12-30 17:39:56 +0530

    socket connected

--- client/src/components/Chatbox.jsx
+++ client/src/components/Chatbox.jsx
@@ -1,37 +1,45 @@
-import {React,useState} from 'react'
+import { React, useState } from 'react'
 import "./chatbox.css"
+import {io} from "socket.io-client"  
+
 function Chatbox() {
+
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState('');
-  
+   const socket = io("http://localhost:5000")
+     socket.on("connection", () => {
+    console.log("Connected with socket ID:", socket.id);
+  });
+
+     
     const handleSendMessage = () => {
-      if (newMessage.trim() === '') return;
-  
-      setMessages([...messages, { text: newMessage, sender: 'user' }]);
-      setNewMessage('');
+        if (newMessage.trim() === '') return;
+
+        setMessages([...messages, { text: newMessage, sender: 'user' }]);
+        setNewMessage('');
     };
-  
+
     return (
-      <div className="App">
-        <div className="chat-box">
-          <div className="messages">
-            {messages.map((message, index) => (
-              <div key={index} className={message.sender === 'user' ? 'user-message' : 'other-message'}>
-                {message.text}
-              </div>
-            ))}
-          </div>
-          <div className="input-box">
-            <input
-              type="text"
-              placeholder="Type a message..."
-              value={newMessage}
-              onChange={(e) => setNewMessage(e.target.value)}
-            />
-            <button onClick={handleSendMessage}>Send</button>
-          </div>
+        <div className="App">
+            <div className="chat-box">
+                <div className="messages">
+                    {messages.map((message, index) => (
+                        <div key={index} className={message.sender === 'user' ? 'user-message' : 'other-message'}>
+                            {message.text}
+                        </div>
+                    ))}
+                </div>
+                <div className="input-box">
+                    <input
+                        type="text"
+                        placeholder="Type a message..."
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e.target.value)}
+                    />
+                    <button onClick={handleSendMessage}>Send</button>
+                </div>
+            </div>
         </div>
-      </div>
     );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const  mongoconnect = require("./db");
+const mongoconnect = require("./db");
+const cors = require("cors")
 const app = express();
 const port = 5000;
 mongoconnect();
-// Add this middleware to parse JSON in the request body
+app.use(cors())
 app.use(express.json());
 app.use("/auth", require("./auth/User"));
 
 const httpServer = createServer(app);
 
-const io = new Server(httpServer);
+const io = new Server(httpServer,{
+  cors: {
+    origin: "http://localhost:5173", // specify the allowed origin
+    methods: ["GET", "POST"] // specify the allowed HTTP methods
+  }
+});
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
-
+  console.log("Client connected with socket ID:", socket.id);
 });
-// io.attach(httpServer);
-
-// Attach Socket.io to the HTTP server
 
 httpServer.listen(5000, () => {
-  console.log(`chat app listening at http://localhost:${port}`);
+  console.log(`Chat app listening at http://localhost:${port}`);
 });
