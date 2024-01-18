a31c05428ab9bb86e7805db14cc75937e58f2d4c
Jashanwebsite <jashandeepsingh61124@gmail.com>
2024-01-18 15:09:45 +0530

    screens install

--- client_native/App.js
+++ client_native/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import React from "react";
 import LayoutSlider from "./components/layout";
 import ChatMessage from "./components/chat";
